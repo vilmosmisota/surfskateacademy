406c13963a969b279a77b791c3d7286214c14c04
vilmosmisota <misotavilmos@gmail.com>
2022-07-04 23:12:02 +0100

    start booking and add supabase

---
+++ src/pages/booking.tsx
@@ -0,0 +1,5 @@
+import BookingView from "../views/booking/Booking.view";
+
+export default function Booking() {
+  return <BookingView />;
+}
---
+++ src/views/booking/Booking.view.tsx
@@ -0,0 +1,7 @@
+export default function BookingView() {
+  return (
+    <main>
+      <h1>Booking</h1>
+    </main>
+  );
+}
