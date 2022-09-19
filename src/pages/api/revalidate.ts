03bc7ea91ed6ad444cb859c9c2f8369875a5f925
vilmosmisota <misotavilmos@gmail.com>
2022-09-19 19:03:56 +0100

    try revalidate

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -10,7 +10,7 @@ export default async function handler(
   }
 
   try {
-    await res.revalidate("/booking");
+    await res.revalidate("/");
     return res.json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error revalidating");
