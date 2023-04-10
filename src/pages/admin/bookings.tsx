02a22be7e8b5ce1a95ed1a5dbc65c46e7f0111ed
vilmosmisota <misotavilmos@gmail.com>
2023-04-10 23:00:46 +0100

    another trigger

--- src/pages/admin/bookings.tsx
+++ src/pages/admin/bookings.tsx
@@ -1,11 +1,7 @@
 import { GetServerSideProps } from "next";
 import { IAdminBookingItem } from "../../interfaces";
 import { protectedRoute } from "../../provider/auth";
-import {
-  getBookings,
-  getNumOfBookings,
-  getRangedBookings,
-} from "../../provider/getData";
+import { getBookings, getNumOfBookings } from "../../provider/getData";
 import AdminBookingsView from "../../views/admin/bookings/AdminBookings.view";
 
 export default function Bookings({
@@ -22,10 +18,7 @@ export default function Bookings({
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({
-  req,
-  query,
-}) => {
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const user = await protectedRoute({ req });
   if (!user) {
     return { props: {}, redirect: { destination: "/admin/login" } };
